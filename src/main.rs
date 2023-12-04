009bb0501fc07ea079d56bf52a3f461270e7bf06
nolook1 <140320948+nolook1@users.noreply.github.com>
2023-12-04 17:28:44 -0500

    More comments purged
    oh the humanity...

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@ use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 use bevy_fps_counter::FpsCounterPlugin;
 
-// Includes draw.rs file for use
 mod draw;
 use draw::{drawing, draw_lines, draw_setup};
 
-// Function main where application is initialized and run
 fn main() {
-    // Create a new Bevy application
     App::new()
-        .insert_resource(Msaa::Sample4) // Insert a resource for multi-sample anti-aliasing with 4 samples
+        .insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                title: "SVGdraw".to_string(), // Set window title to whatever!
+                title: "SVGdraw".to_string(),
                 ..Default::default()
             }),
             ..Default::default()
         }))
-        // Adds SVG, Shape, FpsCounter plugins and systems, then runs the application
         .add_plugins(bevy_svg::prelude::SvgPlugin)
         .add_plugins(ShapePlugin)
         .add_plugins(FpsCounterPlugin)
@@ -28,35 +24,29 @@ fn main() {
         .run();
 }
 
-// A resource for storing the drawing configuration
 #[derive(Resource, Default)]
 pub struct DrawingConfig {
     translation_speed: f32,
 }
 
-// Setup function to spawn a 2D camera and insert a DrawingConfig resource
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default()); // Spawns 2D camera
+    commands.spawn(Camera2dBundle::default());
     commands.insert_resource(DrawingConfig { 
-        translation_speed: 250.0, // Sets speed of camera movement, WASD controls
+        translation_speed: 250.0,
     });
 }
 
-// A system for moving the camera based on keyboard input
 fn camera_movement_system(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&Camera, &mut Transform)>,
     time: Res<Time>,
     drawingconfig: Res<DrawingConfig>,
 ) {
-    // Iterate over each camera and its transform
     for (_camera, mut transform) in query.iter_mut() {
-        let mut direction = Vec3::ZERO; // Initialize the direction vector
+        let mut direction = Vec3::ZERO;
         
-        // Get the translation speed from the drawing configuration
         let translation_speed = drawingconfig.translation_speed;
         
-        // Update the direction vector based on the pressed keys
         if keyboard_input.pressed(KeyCode::W) {
             direction += Vec3::new(0.0, 1.0, 0.0);
         }
@@ -69,7 +59,6 @@ fn camera_movement_system(
         if keyboard_input.pressed(KeyCode::D) {
             direction += Vec3::new(1.0, 0.0, 0.0);
         }
-        // Update the translation of the transform based on the direction, speed, and delta time
         transform.translation += time.delta_seconds() * translation_speed * direction;
     }
 }
