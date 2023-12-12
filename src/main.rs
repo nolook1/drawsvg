5d94c1dc5e4121943b77447c4722ba78f5fd62bc
nolook1 <140320948+nolook1@users.noreply.github.com>
2023-12-11 18:33:53 -0800

    Updated main.rs
    changed settings plugin to work for main file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, window::PresentMode};
+use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 
 mod draw;
@@ -6,7 +6,9 @@ use draw::{drawing, draw_lines, draw_setup};
 
 mod fps_counter;
 use crate::fps_counter::FpsCounterPlugin;
-use fps_counter::fps_setup;
+
+mod settings;
+use crate::fps_counter::SettingsPlugin;
 
 fn main() {
     App::new()
@@ -14,66 +16,16 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "SVGdraw".to_string(),
-                present_mode: PresentMode::AutoVsync,
                 ..Default::default()
             }),
             ..Default::default()
         }))
-        .add_plugins((bevy_svg::prelude::SvgPlugin, ShapePlugin, FpsCounterPlugin))
+        .add_plugins((bevy_svg::prelude::SvgPlugin, ShapePlugin, FpsCounterPlugin, SettingsPlugin))
         .add_systems(Startup, (draw_setup, main_setup, fps_setup))
-        .add_systems(Update, toggle_vsync)
-        .add_systems(FixedUpdate, (drawing, draw_lines, camera_movement_system))
+        .add_systems(FixedUpdate, (drawing, draw_lines))
         .run();
-}
-
-#[derive(Resource, Default)]
-pub struct DrawingConfig {
-    translation_speed: f32,
 }
 
 fn main_setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    commands.insert_resource(DrawingConfig { 
-        translation_speed: 250.0,
-    });
-}
-
-fn toggle_vsync(input: Res<Input<KeyCode>>, mut windows: Query<&mut Window>) {
-    if input.just_pressed(KeyCode::V) {
-        let mut window = windows.single_mut();
-
-        window.present_mode = if matches!(window.present_mode, PresentMode::AutoVsync) {
-            PresentMode::AutoNoVsync
-        } else {
-            PresentMode::AutoVsync
-        };
-        info!("PRESENT_MODE: {:?}", window.present_mode);
-    }
-}
-
-fn camera_movement_system(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Camera, &mut Transform)>,
-    time: Res<Time>,
-    drawingconfig: Res<DrawingConfig>,
-) {
-    for (_camera, mut transform) in query.iter_mut() {
-        let mut direction = Vec3::ZERO;
-        
-        let translation_speed = drawingconfig.translation_speed;
-        
-        if keyboard_input.pressed(KeyCode::W) {
-            direction += Vec3::new(0.0, 1.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            direction += Vec3::new(0.0, -1.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            direction += Vec3::new(-1.0, 0.0, 0.0);
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            direction += Vec3::new(1.0, 0.0, 0.0);
-        }
-        transform.translation += time.delta_seconds() * translation_speed * direction;
-    }
 }
