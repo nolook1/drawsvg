dace0454d205a6b9dd5379d8929faeed933f010f
nolook1 <140320948+nolook1@users.noreply.github.com>
2025-07-27 21:06:49 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ use bevy_prototype_lyon::prelude::*;
 mod draw;
 use draw::{drawing, draw_lines, draw_setup};
 
-mod fps_counter;
-use crate::fps_counter::FpsCounterPlugin;
-
 mod settings;
 use crate::settings::SettingsPlugin;
 
+mod fps_counter;
+use crate::fps_counter::FpsPlugin;
+
 fn main() {
     App::new()
-        .insert_resource(Msaa::Sample4)
+        //.insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "SVGdraw".to_string(),
@@ -20,12 +20,12 @@ fn main() {
             }),
             ..Default::default()
         }))
-        .add_plugins((bevy_svg::prelude::SvgPlugin, ShapePlugin, FpsCounterPlugin, SettingsPlugin))
+        .add_plugins((bevy_svg::prelude::SvgPlugin, ShapePlugin, SettingsPlugin, FpsPlugin))
         .add_systems(Startup, (draw_setup, main_setup))
         .add_systems(FixedUpdate, (drawing, draw_lines))
         .run();
 }
 
 fn main_setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d::default());
 }
