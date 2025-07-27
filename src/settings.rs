15f0cfbe4ecb0e7db30abb29084f3fdf6dc04623
nolook1 <140320948+nolook1@users.noreply.github.com>
2025-07-27 21:07:15 +0200

    Update settings.rs

--- src/settings.rs
+++ src/settings.rs
@@ -1,11 +1,11 @@
-use bevy::{prelude::*, window::PresentMode};
+use bevy::{prelude::*};
 
 pub(super) struct SettingsPlugin;
 
 impl Plugin for SettingsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, settings_setup)
-            .add_systems(Update, (toggle_vsync, camera_movement_system));
+            .add_systems(Update, camera_movement_system);
     }
 }
 
@@ -20,42 +20,28 @@ fn settings_setup(mut commands: Commands) {
     });
 }
 
-fn toggle_vsync(input: Res<Input<KeyCode>>, mut windows: Query<&mut Window>) {
-    if input.just_pressed(KeyCode::V) {
-        let mut window = windows.single_mut();
-
-        window.present_mode = if matches!(window.present_mode, PresentMode::AutoVsync) {
-            PresentMode::AutoNoVsync
-        } else {
-            PresentMode::AutoVsync
-        };
-        info!("PRESENT_MODE: {:?}", window.present_mode);
-    }
-}
-
 fn camera_movement_system(
-    keyboard_input: Res<Input<KeyCode>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&Camera, &mut Transform)>,
     time: Res<Time>,
     drawingconfig: Res<DrawingConfig>,
 ) {
     for (_camera, mut transform) in query.iter_mut() {
         let mut direction = Vec3::ZERO;
-        
         let translation_speed = drawingconfig.translation_speed;
         
-        if keyboard_input.pressed(KeyCode::W) {
+        if keyboard_input.pressed(KeyCode::KeyW) {
             direction += Vec3::new(0.0, 1.0, 0.0);
         }
-        if keyboard_input.pressed(KeyCode::S) {
+        if keyboard_input.pressed(KeyCode::KeyS) {
             direction += Vec3::new(0.0, -1.0, 0.0);
         }
-        if keyboard_input.pressed(KeyCode::A) {
+        if keyboard_input.pressed(KeyCode::KeyA) {
             direction += Vec3::new(-1.0, 0.0, 0.0);
         }
-        if keyboard_input.pressed(KeyCode::D) {
+        if keyboard_input.pressed(KeyCode::KeyD) {
             direction += Vec3::new(1.0, 0.0, 0.0);
         }
-        transform.translation += time.delta_seconds() * translation_speed * direction;
+        transform.translation += time.delta_secs() * translation_speed * direction;
     }
 }
